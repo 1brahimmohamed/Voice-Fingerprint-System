254f541c1fa397dfd510aa0ecb0b3d06acfdf0f1
Ibrahim <imimazim7@gmail.com>
2022-12-07 21:51:58 +0200

    feat: added mp3 file converstion to wav

--- vrs-server/apis/views.py
+++ vrs-server/apis/views.py
@@ -17,6 +17,7 @@ def predict(request):
         mp3 = request.FILES['file']
         print(request.FILES['file'])
 
+        path = convert_to_Wav(mp3)
         # PRE-PROCESSING (feature extraction) #
 
         audio, sr = librosa.load(path, duration=3)
@@ -25,7 +26,7 @@ def predict(request):
         rms = librosa.feature.rms(y=audio)
         zcr = librosa.feature.zero_crossing_rate(audio)
         mfcc = librosa.feature.mfcc(y=audio, sr=sr)
-        # dataframe rowgit
+        # dataframe row
         row_data = f'{np.mean(rms)} {np.mean(zcr)}'
 
         # add mfcc features
@@ -37,3 +38,9 @@ def predict(request):
         return HttpResponse([prediction])
 
 
+def convert_to_Wav(mp3_file):
+    dist = 'operating.wav'
+    sound = AudioSegment.from_mp3(mp3_file)
+    sound.export(dist, format="wav")
+
+    return dist
